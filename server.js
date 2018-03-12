b22946939e905876070eb9c77ef164f0c83c10f2
Justin Licari <Justin@Justins-MacBook-Air.local>
2018-03-12 09:27:27 -0400

    save mailgun-js package

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
 //app.use(express.static(__dirname + '/welcome'/* + '/dist'*/));
-app.use(express.static(__dirname + 'build'));
+app.use(express.static(__dirname + '/build'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
