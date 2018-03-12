8fd2facf56fcf9a0239333769874f06fe95a7b28
Justin Licari <Justin@Justins-MacBook-Air.local>
2018-03-12 18:56:05 -0400

    remove timeout

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
       
       this.state = {
           user: null,      		
-      		foodItemField: 'please enter a food item'
+      		foodItemField: 'Please enter a food item'
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.login = this.login.bind(this); 
