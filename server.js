a9d3054db7bfd06e0d77d2e1943ee2f8c9cb9c90
Justin Licari <Justin@Justins-MacBook-Air.local>
2018-03-22 12:15:53 -0400

    assets for informational site

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(forceSSL());
 
 // Run the app by serving the static files
 // in the dist directory
-//app.use(express.static(__dirname + '/welcome'/* + '/dist'*/));
+app.use(express.static(__dirname + '/welcome'/* + '/dist'*/));
 app.use(express.static(__dirname + '/build'));
 
 // For all GET requests, send back index.html
